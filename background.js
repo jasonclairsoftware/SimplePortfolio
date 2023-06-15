3ee5c43c1420843385834306f2fc6972b643dac7
Jason Clair <jasonclairgamedev@gmail.com>
2023-06-14 20:32:22 -0700

    Got the basic firwork.js working

--- background.js
+++ background.js
@@ -1,50 +1,51 @@
+{
+    // Getting the canvas and the context
+    let canvas = document.getElementById("canvas");
+    let ctx = canvas.getContext('2d');
 
-// Getting the canvas and the context
-let canvas = document.getElementById("canvas");
-let ctx = canvas.getContext('2d');
-
-// Setting the canvas to fit the screen
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-// Init screen to black
-ctx.beginPath();
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-ctx.closePath();
-
-
-/*
-
-THIS IS A TEST CIRCLE. DELETE WHEN FINISHED.
-
-
-ctx.beginPath();
-ctx.fillStyle = 'white';
-ctx.arc(200, 200, 80, 0, Math.PI*2, false);
-ctx.fill();
-ctx.closePath();
-*/
-// Making the canvas responsive
-window.addEventListener('resize', () => {
+    // Setting the canvas to fit the screen
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
+    // Init screen to black
     ctx.beginPath();
-    ctx.fillStyle = 'black';
+    ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.closePath();
-});
 
 
-backgroundLoop();
-function backgroundLoop() {
+    /*
+    
+    THIS IS A TEST CIRCLE. DELETE WHEN FINISHED.
+    */
 
     ctx.beginPath();
-    ctx.fillStyle = 'rgba(0,0,0,0.1)';
-    ctx.fillRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle = 'white';
+    ctx.arc(200, 200, 80, 0, Math.PI * 2, false);
+    ctx.fill();
     ctx.closePath();
 
-    // Callback
-    requestAnimationFrame(backgroundLoop);
-};
+    // Making the canvas responsive
+    window.addEventListener('resize', () => {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        ctx.beginPath();
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.closePath();
+    });
+
+
+    backgroundLoop();
+    function backgroundLoop() {
+
+        ctx.beginPath();
+        ctx.fillStyle = 'rgba(0,0,0,0.1)';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.closePath();
+
+        // Callback
+        requestAnimationFrame(backgroundLoop);
+    };
+}
--- firework.js
+++ firework.js
@@ -1,48 +1,95 @@
-export class Firework {
+
+// Helper class
+class Firework {
 
     // Properties
     posX = 0;
     posY = 0;
     detonationlocation = 0;
-    velocity = [0, 2]; // X, Y Velocity
-    hasdetonated = false;
-    particles = [];
+    startvelocity = [];
+    velocity = [];
+    gravity = 0.05;
 
     // CTOR
-    constructor(posX, posY,detloc) {
+    constructor(posX, posY, detloc) {
         this.posX = posX;
         this.posY = posY;
         this.detonationlocation = detloc;
+        this.startvelocity[0] = 0;
+        this.startvelocity[1] = -(((7.5-5.5)*Math.random())+5); // between 5 and 7.5 seems good
+        this.velocity[0] = this.startvelocity[0];
+        this.velocity[1] = this.startvelocity[1];
     }
 
-
     // Methods
-    update() {
-
-        // Detonate
-
-        if (this.hasdetonated) {
-            // TODO:: Update the particles
-        } else if (this.posY >= this.detonationlocation && this.hasdetonated == false) {
-            // Kaboom!
-            // TODO:: Hide self and render particles
-            this.hasdetonated = true;
-        } else {
-            // Update shell location
-            this.posX += this.velocity[0];
-            this.posY -= this.velocity[1]; // Negative to go up.
+    update(ctx) {
+        // Kaboom
+        if (this.posY < this.detonationlocation || this.velocity[1] > 0) {
+            this.detonate(ctx);
+            return;
         }
+        this.velocity[1] += this.gravity;
+        this.posY += this.velocity[1];
     }
 
     draw(ctx) {
-        if (this.hasdetonated) {
-            ctx.beginPath();
-            ctx.fillStyle = 'white';
-            ctx.arc(this.posX, this.posY, 1, 0, Math.PI * 2, false);
-            ctx.fill();
-            ctx.closePath();
-        } else {
-            // TODO:: Render the particles
-        }
+        ctx.beginPath();
+        ctx.fillStyle = 'white';
+        ctx.arc(this.posX, this.posY, 1, 0, Math.PI * 2, false);
+        ctx.fill();
+        ctx.closePath();
     }
+
+    resetLocation() {
+        this.posX = Math.random() * canvas.width;
+        this.posY = canvas.height;
+        this.detonationlocation = (canvas.height * 0.5) * Math.random();
+        this.velocity[0] = this.startvelocity[0];
+        this.velocity[1] = this.startvelocity[1];
+    }
+
+    detonate(ctx) {
+        ctx.beginPath();
+        ctx.fillStyle = "yellow";
+        ctx.arc(this.posX, this.posY, 10, 0, Math.PI * 2, false);
+        ctx.fill();
+        ctx.closePath();
+        this.resetLocation();
+    }
+
+
 }
+
+
+
+
+{
+    // Getting the canvas context
+    let canvas = document.getElementById("canvas");
+    let ctx = canvas.getContext('2d');
+
+    // Properties
+    let fireworksnumber = 10;
+
+    let fireworks = []
+
+    for (let i = 0; i < fireworksnumber; i++) {
+        fireworks[i] = new Firework(Math.random() * canvas.width, canvas.height, (canvas.height * 0.5) * Math.random());
+    }
+
+    animationLoop();
+    function animationLoop() {
+
+        for (let i = 0; i < fireworksnumber; i++) {
+
+            fireworks[i].update(ctx);
+            fireworks[i].draw(ctx);
+        }
+
+        // Callback
+        window.requestAnimationFrame(animationLoop);
+    }
+
+
+}
+
